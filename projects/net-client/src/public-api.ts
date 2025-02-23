d7823bf1fb8d1fa745a77cc237a4c07169b217bc
pavelo8501 <pavelo8501@gmail.com>
2025-02-23 14:46:07 +0200

    adjusted exports

--- projects/net-client/src/public-api.ts
+++ projects/net-client/src/public-api.ts
@@ -4,7 +4,11 @@
 
 export * from './lib/rest-client/rest-client.service';
 export * from './lib/rest-client/rest-client.providers'
-
+export * from './lib/rest-client/classes/config/rest-client-config'
+export * from './lib/rest-client/classes/rest-client-connection'
+export * from './lib/rest-client/enums/error-code'
+export * from './lib/rest-client/enums/header-key'
+export * from './lib/rest-client/enums/rest-methos'
 
 export * from './lib/ws-client/web-socket-service.service';
 
